bec695e76e3445c03211a80cb7a4b9ceca64cb68
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2023-05-23 00:21:51 +0100

    Day 1 - Part 1

---
+++ questions/2015/01/script.ts
@@ -0,0 +1,38 @@
+import { getFileData, runTest } from "../../../utilities";
+
+import type { Test } from "../../../types/test";
+
+const input = getFileData("./questions/2015/01/input.txt");
+
+// ( === up
+// ) === down
+
+const test: Test[] = [
+	{ case: "(())", answer: 0 },
+	{ case: "()()", answer: 0 },
+	{ case: "(((", answer: 3 },
+	{ case: "(()(()(", answer: 3 },
+	{ case: "))(((((", answer: 3 },
+	{ case: "())", answer: -1 },
+	{ case: "))(", answer: -1 },
+	{ case: ")))", answer: -3 },
+	{ case: ")())())", answer: -3 },
+];
+
+const countFloors = (data: string) => {
+	const totalFloors = data.length;
+	const totalUp = data.split(")").join("").length;
+	const totalDown = totalFloors - totalUp;
+	const floor = 0 + totalUp - totalDown;
+
+	// console.log(`Total Floors: ${totalFloors}`);
+	// console.log(`Total Steps Up: ${totalUp}`);
+	// console.log(`Total Steps Down: ${totalDown}`);
+	// console.log(`Floor ${floor}`);
+
+	return floor;
+};
+
+runTest(test, countFloors);
+
+console.log(countFloors(input));
